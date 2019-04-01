8ddc7706e8a88ba1f4bbfa2f31db583f35b2d9dc
eshinfg <eshinx1@gmail.com>
2019-03-31 21:13:22 -0700

    [Eric] reducers and actions

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,24 +1,26 @@
 import React from "react";
 import { Field, reduxForm } from "redux-form";
+import { connect } from "react-redux";
+import { createStream } from "../../actions";
 
 class StreamCreate extends React.Component {
-  renderError = ({error, touched}) => {
-    if(touched && error) {
-      return(
+  renderError = ({ error, touched }) => {
+    if (touched && error) {
+      return (
         <div className="ui error message">
           <div className="header">{error}</div>
         </div>
-      )
+      );
     }
-  }
+  };
 
-  renderInput= ({ input, label, meta }) => {
-    const className= `field ${meta.error && meta.touched ? 'error' : ''}`
+  renderInput = ({ input, label, meta }) => {
+    const className = `field ${meta.error && meta.touched ? "error" : ""}`;
     console.log(meta);
     return (
       <div className={className}>
         <label>{label}</label>
-        <input {...input} autoComplete="off" /> 
+        <input {...input} autoComplete="off" />
         {this.renderError(meta)}
       </div>
     );
@@ -28,11 +30,12 @@ class StreamCreate extends React.Component {
     //     value={formProps.input.value}
     //   />
     // );
-  }
+  };
 
-  onSubmit(formValues) {
+  onSubmit = formValues => {
+    this.props.createStream(formValues);
     console.log(formValues);
-  }
+  };
 
   render() {
     console.log(this.props);
@@ -67,7 +70,12 @@ const validate = formValues => {
   return errors;
 };
 
-export default reduxForm({
+const formWrapped = reduxForm({
   form: "streamCreate",
   validate
 })(StreamCreate);
+
+export default connect(
+  null,
+  { createStream }
+)(formWrapped);
